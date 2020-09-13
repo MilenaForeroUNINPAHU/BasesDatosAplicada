d9d11600bb1c2b6841cbd62e2ef6e9a05bda4769
Andrew <ramirezandrew172@gmail.com>
2020-09-13 04:32:29 -0500

    Archivos sql

---
+++ ModificacionesTablas.sql
@@ -0,0 +1,15 @@
+use dbcitashospital;
+
+ALTER TABLE cita ADD cantidadCitas int;
+
+ALTER TABLE paciente ADD edad int not null;
+
+ALTER TABLE paciente DROP column apellidoPaciente;
+
+ALTER TABLE paciente modify nombrePaciente varchar(30);
+
+ALTER TABLE paciente change nombrePaciente namePaciente varchar(30);
+
+ALTER TABLE paciente 
+	change nombrePaciente patientName varchar(30),
+    add patientLastName varchar(30)
